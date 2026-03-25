afb67b72d081da88b44025862f9b9b4a3044eb15
Michail Tsagris <mtsagris@yahoo.gr>
2026-03-25 21:43:37 +0200

    change

--- R/rdcor.test.R
+++ R/rdcor.test.R
@@ -5,7 +5,7 @@ rdcor.test <- function(y, x, B = 499) {
     Ry <- Rfast::Rank(y) / n
     Rx <- Rfast::Rank(x) / n
     r <- dcov::dcor(Ry, Rx, type = "V")
-    px <- replicate( B, Rfast2::Sample(x, n) )
+    px <- replicate( B, rangen::Sample(x, n) )
     Rpx <- Rfast::colRanks(px) / n
     pr <- dcov::mdcor(y, Rpx, type = "V")
     res <- c( r, ( sum( pr > r ) + 1 ) / (B + 1) )
@@ -16,7 +16,7 @@ rdcor.test <- function(y, x, B = 499) {
     Ry <- Rfast::Rank(y) / n
     Rx <- Rfast::colRanks(x) / n
     r <- as.vector( dcov::mdcor(Ry, Rx, type = "V") )
-    py <- replicate( B, Rfast2::Sample(y, n) )
+    py <- replicate( B, rangen::Sample(y, n) )
     Rpy <- Rfast::colRanks(py) / n
     pr <- matrix(nrow = B, ncol = dim(x)[2])
     for (i in 1:B)  pr[i, ] <- dcov::mdcor(Rpy[, i], Rx, type = "V")
